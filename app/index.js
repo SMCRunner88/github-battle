4afe63ca6d0056b47c0cf7b4044b32e3df8ba977
Scott Cook <smcrunner88@gmail.com>
2017-04-18 08:08:31 -0700

    I feel dirty

---
+++ app/index.js
@@ -0,0 +1,19 @@
+var React = Require('react');
+var ReactDOM = Require('react-dom');
+require('/.index.css');
+
+class App extends React.Component {
+  render() {
+    return ( 
+			<div>
+      Hello world!
+      </div>
+    )
+  }
+}
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app');
+);
