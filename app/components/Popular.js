f3b0db0a7291ed8ee74df61256dcfd6f3edc3443
smcrunner88 <smcrunner88@gmail.com>
2017-05-01 21:51:47 -0700

    Updating language state

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,17 +1,36 @@
 var React = require('react');
 
 class Popular extends React.Component {
+	constructor(props) {
+		super()
+		this.state = {
+			selectedLanguage: "All"
+		};
+		this.updateLanguage = this.updateLanguage.bind(this);
+	}
+	updateLanguage(lang) {
+		this.setState(function () {
+			return {
+				selectedLanguage: lang,
+			}
+		});
+	}
 	render() {
 		var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+
 		return (
-			<ul className='container'>
+			<ul className='languages'>
+				<p>Selected Languages: {this.state.selectedLanguage}</p>
 				{languages.map(function(lang) {
 					return (
-						<li>
+						<li
+							style={lang === this.state.selectedLanguage ? { color: '#d0021b'}: null}
+							onClick={this.updateLanguage.bind(null, lang)}
+							key={lang}>
 							{lang}
 						</li>
 					)
-				})}
+				}, this)}
 			</ul>
 		)
 	}
