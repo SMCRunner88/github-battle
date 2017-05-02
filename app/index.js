042ac20d501c6b7f87c5cdc4c1c0ea43231eef50
smcrunner88 <smcrunner88@gmail.com>
2017-05-01 17:17:36 -0700

    Called new component directory

--- app/index.js
+++ app/index.js
@@ -1,16 +1,10 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+import PropTypes from 'prop-types';
 require('./index.css');
-
-class App extends React.Component {
-  render() {
-    return ( 
-			<div>Hello world!</div>
-    )
-  }
-}
+var App = require('./components/App')
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('app')
+  <App />, 
+  document.getElementById("app")
 );
