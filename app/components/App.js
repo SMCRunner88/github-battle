4f6b4e46b71bb9ee74931e52cee5a5a701ebc5d9
Scott Cook <smcrunner88@gmail.com>
2017-07-06 07:56:36 -0700

    Button and nav bar styles added

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,36 @@
 var React = require('react');
+
+// React Router
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+var Switch = ReactRouter.Switch;
+
+// Custom Components
+var Nav = require('./Nav');
 var Popular = require('./Popular');
+var Home = require('./Home');
+var Battle = require('./Battle');
 
 class App extends React.Component {
   render() {
     return (
-      <div className='container'>
-        <Popular />
-      </div>
+
+      <Router>
+        <div className='container'>
+          <Nav />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path="/battle" component={Battle} />
+            <Route path='/popular' component={Popular} />
+            <Route render={function() {
+              return <p>Not Found</p>
+            }} />
+          </Switch>
+
+        </div>
+      </Router>
+
     )
   }
 }
