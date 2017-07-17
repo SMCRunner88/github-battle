e7783bf7cba7083e436265b82ed9c318bf380e91
Scott Cook <smcrunner88@gmail.com>
2017-07-17 12:20:56 -0700

    Require result component

--- app/components/App.js
+++ app/components/App.js
@@ -11,6 +11,7 @@ var Nav = require('./Nav');
 var Popular = require('./Popular');
 var Home = require('./Home');
 var Battle = require('./Battle');
+var Results = require('./Results');
 
 class App extends React.Component {
   render() {
@@ -22,6 +23,7 @@ class App extends React.Component {
           <Switch>
             <Route exact path='/' component={Home} />
             <Route exact path="/battle" component={Battle} />
+            <Route path='/battle/results' component={Results} />
             <Route path='/popular' component={Popular} />
             <Route render={function() {
               return <p>Not Found</p>
